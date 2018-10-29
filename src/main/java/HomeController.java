bee00780edd947cf8c6cc6f3f67d8459c2131d1b
bilu-Blen <azblen1@gmail.com>
2018-10-29 16:41:59 -0400

    Initial commit

---
+++ src/main/java/HomeController.java
@@ -0,0 +1,11 @@
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@org.springframework.stereotype.Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home(){
+        return "index";
+    }
+}
